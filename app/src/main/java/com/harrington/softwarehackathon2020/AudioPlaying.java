8e3a1e49231095fcd60187d109bc1741d19fe9ba
Cole Harrington <harringtoncol17@gmail.com>
2020-02-22 18:45:59 -0800

    Still Crashes when running file but because files not being found 6:45 2/22

--- app/src/main/java/com/harrington/softwarehackathon2020/AudioPlaying.java
+++ app/src/main/java/com/harrington/softwarehackathon2020/AudioPlaying.java
@@ -2,16 +2,23 @@ package com.harrington.softwarehackathon2020;
 
 import android.Manifest;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.core.app.ActivityCompat;
 
+import java.io.File;
 import java.io.IOException;
 
 import static android.os.Environment.getExternalStorageDirectory;
 
 public class AudioPlaying {
 
+
+
+    private Spinner spFrequency;
 
     public void run() {
         int n = 1;
@@ -22,7 +29,9 @@ public class AudioPlaying {
 
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
 
-        mediaRecorder.setOutputFile(getExternalStorageDirectory().getAbsolutePath() + "/inputFile.txt");
+
+
+       // mediaRecorder.setOutputFile(getExternalStorageDirectory().getAbsolutePath() + "/inputFile.txt");
 
         mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
